cc96e6235a4bbf4b7bb90cf55280aecfe42ebe1e
Siddhartha Saxena <siddharthasaxena.1998@gmail.com>
2021-01-04 18:21:49 +0530

    Adding Character Cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Header from './components/ui/Header';
+import Character from './components/characters/Character';
+import Search from './components/ui/Search';
+import axios from 'axios';
 
 function App() {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios("https://www.breakingbadapi.com/api/characters")
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    }
+    fetchItems();
+  }, []);
+
   return (
     <div className="container">
-      <h1>Breaking Bad API Example.</h1>
+      <Header />
+      <Search />
+      <Character isLoading={isLoading} items={items} />
     </div>
   );
 }
---
+++ src/components/characters/Character.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CharacterItem from './CharacterItem';
+
+
+const Character = ({ isLoading, items }) => {
+    return isLoading ? (<h1>Loading</h1>) : (
+        <section className="cards">
+            {items.map(item => (
+                <CharacterItem key={item.char_id} item={item} ></CharacterItem>
+            ))}
+        </section>
+    )
+}
+
+export default Character;
